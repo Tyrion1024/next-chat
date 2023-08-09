479dc3544c05cc631381955e0bff5abe0c571975
lizhiwei <lizhiwei.uh@haier.com>
2023-08-09 09:19:56 +0800

    fix: mongodb bug

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,8 +10,8 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(Prisma),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: (process.env.NODE_ENV === 'development' ? process.env.GITHUB_ID_DEV : process.env.GITHUB_ID) as string,
+      clientSecret: (process.env.NODE_ENV === 'development' ? process.env.GITHUB_SECRET_DEV : process.env.GITHUB_SECRET) as string,
       httpOptions: {
         timeout: process.env.NODE_ENV === 'development' ? 60000 : 10000,
       }
